3bc37aef9185c97b2838414003483eac740d94ed
mariocla <121092135+mariocla@users.noreply.github.com>
2025-03-03 17:10:55 +0100

    Merge Cambios

---
+++ src/app/pages/podcaster/podcaster.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-podcaster',
+  imports: [],
+  template: `
+    <p>
+      podcaster works!
+    </p>
+  `,
+  styles: ``
+})
+export class PodcasterComponent {
+
+}
